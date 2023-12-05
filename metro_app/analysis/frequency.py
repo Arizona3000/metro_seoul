c195b1046822a1a123abfbd9316b33f317cda9fd
cd <celeste2valroger@gmail.com>
2023-12-05 13:05:47 +0100

    change in frequency graph function

--- metro_app/analysis/frequency.py
+++ metro_app/analysis/frequency.py
@@ -28,26 +28,42 @@ def get_frequency_graph(df, date_for_average: str, date_for_pred: str, day: str,
 
     frequency = get_data_frequency(df_for_avegarge, date_for_average)
     frequency_ss = frequency[(frequency['day']==day) & (frequency['station_name'] == station) & (frequency['line']==line)]
+    frequency_ss = frequency_ss.reset_index()
+    print(frequency_ss)
+
 
     prepro_df =general_preprocessing(df_for_pred)
-    seouls_s = prepro_df[(prepro_df['date'] == date_for_pred) & (prepro_df['station_name']==station) & prepro_df['line']==line]
+    seouls_s = prepro_df[(prepro_df['date'] == date_for_pred) & (prepro_df['station_name']==station) & (prepro_df['line']==line)]
     seouls_station = seouls_s.groupby(['date','station_name'])[seouls_s.columns[5:]].sum().reset_index()
+    print(seouls_station)
 
-    fig = px.line(x=seouls_station.columns[2:],
-              y=list(seouls_station.iloc[0].values[2:]),
-              color=px.Constant("Day"),
-              line_shape='spline',
-              color_discrete_sequence=['darkblue'])
+    # Create a grouped bar chart using plotly.graph_objects
+    fig = go.Figure()
 
-    fig.add_bar(x=frequency_ss.columns[4:],
-                y=list(frequency_ss.iloc[0].values[4:]),
-                name="Average",
-                marker_color='lightgray')
+    # Add bars for the average on the right
+    fig.add_trace(go.Bar(
+        x=frequency_ss.columns[5:],
+        y=list(frequency_ss.iloc[0].values[5:]),
+        name="Average",
+        marker_color='lightgray',
+        offset=-0.2  # Offset to position on the right
+    ))
 
+    # Add bars for the prediction on the left
+    fig.add_trace(go.Bar(
+        x=seouls_station.columns[5:],
+        y=list(seouls_station.iloc[0].values[5:]),
+        name="Prediction",
+        marker_color='darkblue',
+        offset=0.2  # Offset to position on the left
+    ))
+
+    # Update layout
     fig.update_layout(
         xaxis_title='Time of the day (h)',
         yaxis_title='Number of people',
-        title=f'Fréquentation moyenne le vendredi a {frequency_ss.station_name.values[0]}',
+        title=f'Average Crowding and Prediction on {day} at {station}',
+        barmode='group',  # Grouped bar chart
         plot_bgcolor='rgba(255, 255, 255, 0.9)',
         paper_bgcolor='rgba(255, 255, 255, 0.9)',
         font=dict(color='black'),
