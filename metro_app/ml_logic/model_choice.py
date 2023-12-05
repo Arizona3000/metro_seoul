704a0e735a48a62e70e89d25caf4d45c7556e690
victor lintermans <victorlintermans@gmail.com>
2023-12-05 01:15:27 +0100

    ajout api

--- metro_app/ml_logic/model_choice.py
+++ metro_app/ml_logic/model_choice.py
@@ -15,86 +15,107 @@ from preprocess import general_preprocessing, model_data_preprocessing
 from model import prophet_train_predict
 from gcp.setup import upload, upload_from_file
 
-def choose_model(df, station_list, number_hours, key_to_json):
+def choose_model(df, number_hours, key_to_json):
 
     df = general_preprocessing(df)
     df = model_data_preprocessing(df)
 
+    station_list = [list(row) for row in df[['station_name', 'line']].values]
+    unique_list = [list(item) for item in set(tuple(row) for row in station_list)]
     df = df.groupby(['datetime', 'station_name', 'station_number', 'line'])['value'].sum().reset_index()
 
-    for station, station_line in station_list:
+    list_mape = []
 
-        df_station = df[(df['station_name'] == station.strip()) & (df['line'] == int(station_line))]
+    for station, station_line in unique_list:
 
-        ## MSTL
+        try :
 
-        df_model_mstl = df_station[['station_name', 'datetime', 'value']]
-        df_model_mstl.rename(columns={'station_name':'unique_id', 'datetime':'ds', 'value':'y'}, inplace=True)
+            df_station = df[(df['station_name'] == station.strip()) & (df['line'] == int(station_line))]
 
-        train_size = int(len(df_model_mstl) * 0.8)
-        train, test = df_model_mstl.iloc[:train_size, :], df_model_mstl.iloc[train_size:, :]
+            ## MSTL
 
-        models = [MSTL(
-            season_length=[24, 24 * 7, 24 * 7 * 2, 24 * 7 * 52], # seasonalities of the time series  ##Ajouter 24*7*365,25
-            trend_forecaster=AutoARIMA()
-        )]
+            # df_model_mstl = df_station[['station_name', 'datetime', 'value']]
+            # df_model_mstl.rename(columns={'station_name':'unique_id', 'datetime':'ds', 'value':'y'}, inplace=True)
 
-        sf = StatsForecast(
-            models=models,
-            df=train,
-            freq='H',
-            n_jobs=-1,
-            )
+            # train_size = int(len(df_model_mstl) * 0.8)
+            # train, test = df_model_mstl.iloc[:train_size, :], df_model_mstl.iloc[train_size:, :]
 
-        sf.fit(train)
+            # models = [MSTL(
+            #     season_length=[24, 24 * 7, 24 * 7 * 2, 24 * 7 * 52], # seasonalities of the time series  ##Ajouter 24*7*365,25
+            #     trend_forecaster=AutoARIMA()
+            # )]
 
-        forecast = sf.predict(h = number_hours)
+            # sf = StatsForecast(
+            #     models=models,
+            #     df=train,
+            #     freq='H',
+            #     n_jobs=-1,
+            #     )
 
-        station_number = df_station.station_number.unique()[0]
+            # sf.fit(train)
 
-        forecast2 = forecast.copy()
-        forecast2['MSTL'] = forecast2.apply(lambda row: 0 if 0 <= row['ds'].hour <= 4 else row['MSTL'], axis=1)
+            # forecast = sf.predict(h = number_hours)
 
-        forecast2['station_number'] = station_number
+            station_number = df_station.station_number.unique()[0]
 
-        mape_mstl = mape(forecast2['MSTL'].values, test.head(number_hours)['y'].values)
+            # forecast2 = forecast.copy()
+            # forecast2['MSTL'] = forecast2.apply(lambda row: 0 if 0 <= row['ds'].hour <= 4 else row['MSTL'], axis=1)
 
+            # forecast2['station_number'] = station_number
 
-        ## Prophet
+            # mape_mstl = mape(forecast2['MSTL'].values, test.head(number_hours)['y'].values)
 
-        model, mape_prophet, prediction = prophet_train_predict(df_station, days=3,
-                                                                changepoint_prior_scale=0.2, seasonality_prior_scale=10.0,
-                                                                holidays_prior_scale=10.0, seasonality_mode='multiplicative')
+            #mape_mstl = 1
+            ## Prophet
 
-        prediction_prophet = prediction[['ds', 'yhat']]
+            model, mape_prophet, prediction = prophet_train_predict(df_station, days=3,
+                                                                    changepoint_prior_scale=0.2, seasonality_prior_scale=10.0,
+                                                                    holidays_prior_scale=10.0, seasonality_mode='multiplicative')
 
-        station_name = df_station.station_name.unique()[0]
+            if mape_prophet == 0 :
+                pass
 
+            else:
 
-        prediction_prophet['unique_id'] = station_name
-        prediction_prophet['station_number'] = station_number
+                list_mape.append(mape_prophet)
 
+                prediction_prophet = prediction[['ds', 'yhat']]
 
-        prediction_prophet = prediction[['unique_id', 'ds', 'yhat']]
-
-        prediction_prophet.rename(columns={'yhat':'MSTL'}, inplace=True)
-
-
-
-        ## Station number
-
-        #station_number = df_station.station_number.unique()[0]
+                station_name = df_station.station_name.unique()[0]
 
 
-        if mape_prophet > mape_mstl:
-            #print('yes')
-            serialized_model = pickle.dumps(sf)
-            print(upload(serialized_model,file_name=f'models/model_station_{station_number}.pkl', path_to_json_key=key_to_json))
-            print(upload_from_file(forecast2, file_name=f'data/pred/data_pred_station_{station_number}.pkl', path_to_json_key=key_to_json ))
-            print('File has been uploaded to gcp')
+                prediction_prophet['unique_id'] = str(station).strip()
+                prediction_prophet['station_number'] = station_number
+                prediction_prophet['line'] = station_line
 
-        else:
-            serialized_model = pickle.dumps(model)
-            print(upload(serialized_model,file_name=f'models/model_station_{station_number}.pkl', path_to_json_key=key_to_json))
-            print(upload_from_file(forecast2, file_name=f'data/pred/data_pred_station_{station_number}.pkl', path_to_json_key=key_to_json ))
-            print('File has been uploaded to gcp')
+
+
+                prediction_prophet = prediction_prophet[['unique_id','station_number','line', 'ds', 'yhat']]
+
+                prediction_prophet.rename(columns={'yhat':'MSTL'}, inplace=True)
+
+                print(f'the mape for {station_name} is {mape_prophet}')
+
+                ## Station number
+
+                #station_number = df_station.station_number.unique()[0]
+
+
+                # if mape_prophet > mape_mstl:
+                #     #print('yes')
+                #     serialized_model = pickle.dumps(sf)
+                #     print(upload(serialized_model,file_name=f'models/model_station_{station_number}.pkl', path_to_json_key=key_to_json))
+                #     print(upload_from_file(forecast2, file_name=f'data/pred/data_pred_station_{station_number}.pkl', path_to_json_key=key_to_json ))
+                #     print('File has been uploaded to gcp (mstl)')
+
+                serialized_model = pickle.dumps(model)
+                print(upload(serialized_model,file_name=f'models/model_station_{station_number}.pkl', path_to_json_key=key_to_json))
+                print(upload(prediction_prophet.to_csv(), file_name=f'data/pred/data_pred_station_{station_number}.csv', path_to_json_key=key_to_json ))
+                print('File has been uploaded to gcp (prophet)')
+
+        except:
+            pass
+
+
+    average = sum(list_mape) / len(list_mape)
+    return average
