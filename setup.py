e4d633664ec653c030b244892b0747a2bcf8a599
Thomas Metral <tm.metral@gmail.com>
2023-12-05 15:45:00 +0100

    add setup.py for docker api

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open('requirements.txt') as f:
+    content = f.readlines()
+requirements = [x.strip() for x in content]
+
+setup(name='api',
+      description="predicts metro attendance in Seoul using Prophet",
+      packages=find_packages(),
+      install_requires=requirements)
